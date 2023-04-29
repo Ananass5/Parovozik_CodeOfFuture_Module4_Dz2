4995606f6358ac262b9e2a57d7f1d086600ba59d
Ananas <sub.yar@ya.ru>
2023-04-29 13:43:15 +0300

    Добавил кнопку "купить еще билет"

--- app/src/main/java/com/ananas/serialization_codeoffuture_module4_3/SecondActivity.java
+++ app/src/main/java/com/ananas/serialization_codeoffuture_module4_3/SecondActivity.java
@@ -2,18 +2,24 @@ package com.ananas.serialization_codeoffuture_module4_3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class SecondActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView textView_2act;
     private Railway railway;
+    private Button btnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        btnBack = findViewById(R.id.btnBack);
 
         Bundle bundle = getIntent().getExtras();
 
@@ -26,6 +32,13 @@ public class SecondActivity extends AppCompatActivity {
                 + "Время отправления: " + railway.getTime() + "\n\n"
                 + "Цена билета: " + railway.getCost() + "\n\n");
 
+        btnBack.setOnClickListener(this);
 
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
     }
 }
